870843ae586985b9294e6731cb072c020256efd7
zlray <1411826682@qq.com>
2019-02-18 22:26:16 +0800

    springboot日志框架

--- src/main/java/com/zl/springbootquickdemo/SpringBootQuickDemoApplication.java
+++ src/main/java/com/zl/springbootquickdemo/SpringBootQuickDemoApplication.java
@@ -1,5 +1,7 @@
 package com.zl.springbootquickdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -10,6 +12,7 @@ public class SpringBootQuickDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootQuickDemoApplication.class, args);
+
     }
 
 }
--- src/test/java/com/zl/springbootquickdemo/SpringBootQuickDemoApplicationTests.java
+++ src/test/java/com/zl/springbootquickdemo/SpringBootQuickDemoApplicationTests.java
@@ -3,13 +3,15 @@ package com.zl.springbootquickdemo;
 import com.zl.springbootquickdemo.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 /**
  * SpringBoot单元测试
- *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,9 +20,18 @@ public class SpringBootQuickDemoApplicationTests {
     @Autowired
     Person person;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() {
         System.out.println(person);
+
+        //日志的级别 由低到高  trace debug info warn error
+        logger.trace("trace日志"); //跟踪
+        logger.debug("debug日志"); //调试
+        logger.info("info日志"); //信息
+        logger.warn("warn日志"); //警告
+        logger.error("error日志"); //错误
     }
 }
 
